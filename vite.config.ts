fbdf3fe38771ac1cb15f131f7e93c09a56299d79
Justinsanjp <64624657+justinsanjp@users.noreply.github.com>
2026-03-16 12:04:12 +0100

    Add comment to vite.config.ts for deployment sync
    Added a comment for deployment sync.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// Added comment to force git state sync for deployment
 export default defineConfig({
   plugins: [react()],
 })
